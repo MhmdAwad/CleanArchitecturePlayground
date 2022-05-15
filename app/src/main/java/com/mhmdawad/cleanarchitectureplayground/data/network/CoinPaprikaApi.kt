d5f4559e926eda31fc4215f9f3769fd46293b0f0
Mo Awad <mhmdawaddd@gmail.com>
2022-05-15 16:07:22 +0200

    implement API

---
+++ app/src/main/java/com/mhmdawad/cleanarchitectureplayground/data/network/CoinPaprikaApi.kt
@@ -0,0 +1,16 @@
+package com.mhmdawad.cleanarchitectureplayground.data.network
+
+import com.mhmdawad.cleanarchitectureplayground.data.network.dto.CoinDetailDto
+import com.mhmdawad.cleanarchitectureplayground.data.network.dto.CoinDto
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+
+interface CoinPaprikaApi {
+
+    @GET("/v1/coins")
+    suspend fun getCoins(): List<CoinDto>
+
+    @GET("/v1/coins/{id}")
+    suspend fun getCoinById(@Path("coinId") id: String): CoinDetailDto
+}
---
+++ app/src/main/java/com/mhmdawad/cleanarchitectureplayground/data/network/dto/Whitepaper.kt
@@ -0,0 +1,11 @@
+package com.mhmdawad.cleanarchitectureplayground.data.network.dto
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Whitepaper(
+    @SerializedName("link")
+    val link: String,
+    @SerializedName("thumbnail")
+    val thumbnail: String
+)
